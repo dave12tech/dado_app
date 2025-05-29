da54c58205b9507dbbb557b8f08cd73227921c83
Diego David Martinez <diegodavidmartinez@Diegos-Mac-mini.local>
2025-05-28 22:07:35 -0400

    Startitng_Demo_Dado_App

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 117, 202, 242),
+        body: Center(child: Text('Dado App is coming soon!!!')),
+      ),
+    ),
+  );
+}
