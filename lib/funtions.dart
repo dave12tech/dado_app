ab09e835f56cb6d6db40034f726e462e0fcb2913
Diego David Martinez <diegodavidmartinez@Diegos-Mac-mini.local>
2025-05-29 21:34:55 -0400

    funtion_GradientContainer

---
+++ lib/funtions.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 78, 207, 250),
+            Color.fromARGB(255, 122, 103, 218),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Dado App is coming soon!',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
