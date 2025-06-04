fbfb805b5f5f321ccd42e85acfc5ee162b4c888a
Diego David Martinez <diegodavidmartinez@Diegos-Mac-mini.local>
2025-06-03 21:03:14 -0400

    gradient container updated

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
-}
-
 class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
   @override
   Widget build(context) {
     return Container(
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:dado_app/gradient_container.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 207, 250),
-                Color.fromARGB(255, 122, 103, 218),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Dado App is coming soon!',
-              style: TextStyle(color: Colors.white, fontSize: 30),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(const MaterialApp(home: Scaffold(body: GradientContainer())));
 }
